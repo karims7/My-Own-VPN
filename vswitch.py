5d9ae9c294bc76e5786e7f033642130ff0180d84
karims7 <karims7@my.yorku.ca>
2025-10-05 02:18:50 -0400

    basics

---
+++ vswitch.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Socket lets program talk over the network
+Can send and receive mesages between computers
+This virtual switch program uses this to send and receive UDP packets
+
+sys can read command line arguments, stop the program, print errors etc.
+Used to read port numbers from command line arguments. 
+Ports: number that tells a computer which program a message is meant for. 
+So, IP address tells which computer, port number tells which program on that computer.
+"""
+import socket 
+import sys
+
+"""
+sys.argv: is a list of words after typing "python3". For example, if i type "python3 vswitch.py 8080" then sys.argv = ["vswitch.py", "8080"]. 
+So, the following if statement checks if i typed exactly 2 words after "python3". And stops the program with sys.exit(1).
+The print statement tells the user how to use the program correctly.
+"""
+
+# parse parameters
+server_port = None
+if len(sys.argv) != 2:
+    print(f"Usage: python3 vswitch.py {VSWITCH_PORT}")
+    sys.exit(1)
+else:
+    server_port = int(sys.argv[1]) # sets server_port to the port number in the command line argument
+
+"""
+server_addr is a tuple and can't be altered. 
+0.0.0.0 means listen on all IP on your machine. 
+It tells the program to accept messages sent to any of the computer's IP addresses. "I'm the VSwitch. I will listen for UDP messages on port 8080, no matter which IP they come through."
+""" 
+server_addr = ("0.0.0.0", server_port) 
