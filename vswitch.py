2662e67b23f946e7456133bb5e6341feb24fe393
karims7 <karims7@my.yorku.ca>
2025-10-05 02:44:05 -0400

    Created UDP socket object

--- vswitch.py
+++ vswitch.py
@@ -4,7 +4,7 @@
 """
 Socket lets program talk over the network
 Can send and receive mesages between computers
-This virtual switch program uses this to send and receive UDP packets
+This virtual switch program uses sockets to send and receive UDP packets
 
 sys can read command line arguments, stop the program, print errors etc.
 Used to read port numbers from command line arguments. 
@@ -29,8 +29,28 @@ else:
     server_port = int(sys.argv[1]) # sets server_port to the port number in the command line argument
 
 """
-server_addr is a tuple and can't be altered. 
+server_address is a tuple and can't be altered. 
 0.0.0.0 means listen on all IP on your machine. 
 It tells the program to accept messages sent to any of the computer's IP addresses. "I'm the VSwitch. I will listen for UDP messages on port 8080, no matter which IP they come through."
 """ 
-server_addr = ("0.0.0.0", server_port) 
+server_address = ("0.0.0.0", server_port) 
+
+
+
+
+# 0. create a UDP socket, bind to service port
+"""
+Creating a socket for the program to use for sending and receiving data. ".socket()" constructor method creates a new socket object (from socket class in socket module).
+".socket(family, type)" takes two arguments:
+- family: what kind of address (e.g., IPv4 or IPv6)
+- type: what kind of connection (e.g., TCP or UDP)
+
+both are constants:
+socket.AF_INET: means "use IPv4 addresses" (like 127.0.0.1 or 10.1.1.101)
+socket.SOCK_DGRAM: means "use UDP (User Datagram Protocol),” which sends small packets fast without checking if they arrived.
+"""
+vserver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # vserver_socket is now a UDP socket object.
+vserver_socket.bind(server_address)
+print(f"VSwitch Started at {server_address[0]}:{server_address[1]}")
+
+
